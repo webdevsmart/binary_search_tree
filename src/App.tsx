75a6f6ecebd7d586932ad0efcd8f847921f6aea2
webdevsmart <webdevsmart@hotmail.com>
2021-12-08 04:58:53 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Binary Search Tree Generator</div>
+      <div></div>
+      <div>asdf</div>
+    </div>
+  );
+}
+
+export default App;
